8a2fa9817fee8a4afd47f426495dd8be3fbd63c5
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2025-05-11 14:37:32 +0100

    add getters and setters to Pomodoro class

--- src/AppFrame.java
+++ src/AppFrame.java
@@ -1,7 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class AppFrame extends JFrame {
+    // Global button declarations.
+    JButton startGenericPomodoroButton = new JButton("Start Generic Pomodoro");
+
     public AppFrame() {
         this.setTitle("Flexible Pomodoro");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -9,5 +14,27 @@ public class AppFrame extends JFrame {
         this.setSize(800, 600);
         this.setVisible(true);
         this.getContentPane().setBackground(new Color(0x98BBF5));
+        this.setLayout(null);
+
+        // The title of the application.
+        JLabel appTitle = new JLabel("Flexible Pomodoro");
+        appTitle.setHorizontalTextPosition(JLabel.CENTER);
+        appTitle.setHorizontalAlignment(JLabel.CENTER);
+        appTitle.setVerticalAlignment(JLabel.CENTER);
+        appTitle.setFont(appTitle.getFont().deriveFont(Font.BOLD, 24));
+        appTitle.setBounds(100, 100, 600, 50);
+        this.add(appTitle);
+
+        // Buttons logic.
+        startGenericPomodoroButton.setHorizontalTextPosition(JLabel.CENTER);
+        startGenericPomodoroButton.setHorizontalAlignment(JLabel.CENTER);
+        startGenericPomodoroButton.setVerticalTextPosition(JLabel.CENTER);
+        startGenericPomodoroButton.setVerticalAlignment(JLabel.CENTER);
+        startGenericPomodoroButton.setBounds(300, 200, 200, 50);
+        startGenericPomodoroButton.setFocusable(false);
+        startGenericPomodoroButton.setBackground(new Color(0x30589E));
+        startGenericPomodoroButton.setForeground(Color.WHITE);
+        startGenericPomodoroButton.addActionListener(e -> System.out.println("Hello World!"));
+        this.add(startGenericPomodoroButton);
     }
 }
--- src/Pomodoro.java
+++ src/Pomodoro.java
@@ -1,13 +1,39 @@
 import java.util.ArrayList;
 
 public class Pomodoro {
-    private ArrayList<FocusPeriod> focusPeriods;
-
     public Pomodoro(ArrayList<FocusPeriod> focusPeriods) {
         this.focusPeriods = focusPeriods;
     }
 
+    private ArrayList<FocusPeriod> focusPeriods;
+    private int shortBreakDuration;
+    private int longBreakDuration; // If value is 0, Pomodoro does not have long break.
+
     public int getFocusPeriodsCount() {
         return focusPeriods.size();
+    }
+
+    public ArrayList<FocusPeriod> getFocusPeriods() {
+        return focusPeriods;
+    }
+
+    public void setFocusPeriods(ArrayList<FocusPeriod> focusPeriods) {
+        this.focusPeriods = focusPeriods;
+    }
+
+    public int getShortBreakDuration() {
+        return shortBreakDuration;
+    }
+
+    public void setShortBreakDuration(int shortBreakDuration) {
+        this.shortBreakDuration = shortBreakDuration;
+    }
+
+    public int getLongBreakDuration() {
+        return longBreakDuration;
+    }
+
+    public void setLongBreakDuration(int longBreakDuration) {
+        this.longBreakDuration = longBreakDuration;
     }
 }
